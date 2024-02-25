f5cb09fadfe29a301c4c81283cdda615b7a225ee
Tayfun Yiğit <tayfunyigit02@gmail.com>
2024-02-25 08:06:36 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+// REACT js
 import React from 'react';
+
+// REACT DOM
 import ReactDOM from 'react-dom/client';
+
+// EXTERNAL CSS
 import './index.css';
-import App from './components/_1_Style/Style';
+
+// reportWebVitals
 import reportWebVitals from './reportWebVitals';
 
+
+// COMPONENT
+import _1_Style from './components/_1_Style/Style';
+import JsxTutorials from "./components/_2_Jsx/JsxTutorials"
+
+// ROOT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <_1_Style />  */}
+    <JsxTutorials></JsxTutorials>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
